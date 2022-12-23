da2b28b04382fe6f8ba5328eb97b7619e0a0e290
Vanshdeep Singh <vanshdeepsingh549@gmail.com>
2022-12-23 21:16:12 +0530

    new files added

---
+++ my-app/src/Pages/Components/Box.js
@@ -0,0 +1,11 @@
+import Login_Button from './Login_Button.js'
+import Signup_Button from './Signup_Button.js'
+
+export default function Box(){
+    return( 
+        <div className="box1">
+            <Login_Button/>
+            <Signup_Button/> 
+        </div>
+    );
+}
---
+++ my-app/src/Pages/Promotionpage.js
@@ -0,0 +1,13 @@
+import Box from './Components/Box.js'
+import Header from './Components/Header.js'
+import Footer from './Components/Footer.js'
+
+export default function Promotionpage(){
+    return(
+        <div>
+            <Header/>
+                <Box/>
+            {/* <Footer/> */}
+        </div>
+    );
+}
